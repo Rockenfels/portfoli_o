be313d7660285462b575c1ffbea3769df36f9438
Ray O <52436369+Rockenfels@users.noreply.github.com>
2020-12-17 10:54:15 -0700

    Edited index.html boilerplate; added router to indes.js; Home and NavBar compnents created

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        //Hero graphic
+
+        //Name
+
+        //Project carousel (ie. /projects/id stored in redux store?)
+        <img />
+    )
+}
---
+++ src/Components/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NavBar } from 'react-bootstrap';
+
+const NavBar = () => {
+    return(
+        <NavBar bg="dar" expand="lg" >
+            
+        </NavBar>
+    )
+}
